4dd1a6999abc40c35dfed36c72be848f51a80170
Rico Rojas <djizco@yahoo.com>
2020-09-30 16:07:37 -0500

    Add documentation for Button and Buttons

--- src/Buttons/Buttons.js
+++ src/Buttons/Buttons.js
@@ -3,11 +3,14 @@ import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
 export default function Buttons({
+  addons,
+  align,
   children,
   className,
   size,
   ...props
 }) {
+  // Size
   const sizeMap = {
     small: 'are-small',
     medium: 'are-medium',
@@ -15,7 +18,17 @@ export default function Buttons({
   };
   const isSize = size && sizeMap[size];
 
-  const ButtonsClasses = classNames('buttons', className, isSize);
+  // Align
+  const alignMap = {
+    left: 'is-left',
+    center: 'is-centered',
+    right: 'is-right',
+  };
+  const isAlign = align && alignMap[align];
+
+  const ButtonsClasses = classNames('buttons', className, isSize, isAlign, {
+    'has-addons': addons,
+  });
 
   return (
     <div className={ButtonsClasses} {...props}>
@@ -28,10 +41,14 @@ Buttons.propTypes = {
   className: PropTypes.string,
   children: PropTypes.node,
   size: PropTypes.oneOf(['small', 'medium', 'large']),
+  addons: PropTypes.bool,
+  align: PropTypes.oneOf(['left', 'center', 'right']),
 };
 
 Buttons.defaultProps = {
   className: undefined,
   children: null,
   size: undefined,
+  addons: false,
+  align: undefined,
 };
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 export { default as Block } from './Block';
 export { default as Box } from './Box';
 export { default as Button } from './Button';
+export { default as Buttons } from './Buttons';
+export { default as Content } from './Content';
