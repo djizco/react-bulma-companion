5bcf62c016970ef9fa4000d91ee4d938b82155d7
Rico Rojas <djizco@yahoo.com>
2023-05-14 02:26:24 -0500

    Updates to make package work

---
+++ src/index.d.ts
@@ -0,0 +1,53 @@
+// Elements
+export { default as Block } from './Block';
+export { default as Box } from './Box';
+export { default as Button } from './Button';
+export { default as Buttons } from './Buttons';
+export { default as Content } from './Content';
+export { default as Delete } from './Delete';
+export { default as Element } from './Element';
+export { default as Heading } from './Heading';
+export { default as Icon } from './Icon';
+export { default as Image } from './Image';
+export { default as Loader } from './Loader';
+export { default as Notification } from './Notification';
+export { default as Progress } from './Progress';
+export { default as Table } from './Table';
+export { default as Tag } from './Tag';
+export { default as Tags } from './Tags';
+export { default as Title } from './Title';
+
+// Components
+export { default as Breadcrumb } from './Breadcrumb';
+export { default as Card } from './Card';
+export { default as Dropdown } from './Dropdown';
+export { default as Menu } from './Menu';
+export { default as Message } from './Message';
+export { default as Modal } from './Modal';
+export { default as Navbar } from './Navbar';
+export { default as Pagination } from './Pagination';
+export { default as Panel } from './Panel';
+export { default as Tabs } from './Tabs';
+
+// Form
+export { default as Checkbox } from './Checkbox';
+export { default as Control } from './Control';
+export { default as Field } from './Field';
+export { default as File } from './File';
+export { default as Help } from './Help';
+export { default as Input } from './Input';
+export { default as Label } from './Label';
+export { default as Radio } from './Radio';
+export { default as Select } from './Select';
+export { default as Textarea } from './Textarea';
+
+// Layout
+export { default as Column } from './Column';
+export { default as Columns } from './Columns';
+export { default as Container } from './Container';
+export { default as Footer } from './Footer';
+export { default as Hero } from './Hero';
+export { default as Level } from './Level';
+export { default as Media } from './Media';
+export { default as Section } from './Section';
+export { default as Tile } from './Tile';
