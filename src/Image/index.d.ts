2c131422c73f2f78afb33dc0727a4cb4860bf6e6
Rico Rojas <djizco@yahoo.com>
2023-10-29 21:12:19 -0500

    Add missing Image types

--- src/Image/index.d.ts
+++ src/Image/index.d.ts
@@ -2,7 +2,16 @@ import { ElementComponent } from '../Element';
 
 import ImageContent from './ImageContent';
 
-export interface ImageProps {}
+export type ImageSize = '16x16' | '24x24' | '32x32' | '48x48' | '64x64' | '96x96' | '128x128';
+export type ImageRatio =
+  'square' | '1by1' | '5by4' | '4by3' | '3by2' | '5by3' | '16by9' | '2by1' | '3by1' |
+  '4by5' | '3by4' | '2by3' | '3by5' | '9by16' | '1by2' | '1by3';
+
+export interface ImageProps {
+  fullwidth?: boolean;
+  ratio?: ImageRatio;
+  size?: ImageSize;
+}
 
 declare const Image: ElementComponent<ImageProps, 'figure'> & {
   Content: typeof ImageContent;
